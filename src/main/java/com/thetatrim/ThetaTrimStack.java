b9d812f79bc12875e4789de9c0c93edab7086ff1
tobiasgruber-at <tgruber.at@gmail.com>
2024-01-07 15:47:19 +0100

    feat: add pre-processing-queue (with s3 trigger)

--- src/main/java/com/thetatrim/ThetaTrimStack.java
+++ src/main/java/com/thetatrim/ThetaTrimStack.java
@@ -9,18 +9,30 @@ import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.s3.EventType;
+import software.amazon.awscdk.services.s3.NotificationKeyFilter;
+import software.amazon.awscdk.services.s3.notifications.SqsDestination;
+import software.amazon.awscdk.services.sqs.Queue;
 import software.constructs.Construct;
 
 import java.util.Map;
 
 public class ThetaTrimStack extends Stack {
-    /** S3 Bucket for storing job objects such as videos and frames. */
+    private static final String prefix = "thetatrim-";
+    /**
+     * S3 Bucket for storing job objects such as videos and frames.
+     */
     private Bucket jobObjectBucket;
-    /** Lambda function for creating jobs and presigned urls. */
-    private Function postJobHandler;
-    /** Rest API for handling different endpoints. */
+    /**
+     * Lambda function for creating jobs and presigned urls.
+     */
+    private Function postJobLambda;
+    /**
+     * Rest API for handling different endpoints.
+     */
     private RestApi restApi;
-    
+    private Queue preProcessingQueue;
+
     public ThetaTrimStack(final Construct scope, final String id) {
         this(scope, id, null);
     }
@@ -29,16 +41,20 @@ public class ThetaTrimStack extends Stack {
         super(scope, id, props);
         setupResources();
         grantPermissions();
+        configureTriggers();
         configureEndpoints();
     }
-    
-    /** Initializes all resources of the stack. */
+
+    /**
+     * Initializes all resources of the stack.
+     */
     private void setupResources() {
         jobObjectBucket = Bucket.Builder.create(this, "JobObjectBucket")
-            .bucketName("job-object-bucket")
+            .bucketName(prefix + "job-object-bucket")
             .versioned(true)
             .build();
-        postJobHandler = Function.Builder.create(this, "PostJobHandler")
+        postJobLambda = Function.Builder.create(this, "PostJobHandler")
+            .functionName(prefix + "post-job-handler")
             .runtime(Runtime.PYTHON_3_12)
             .handler("post_job.handler")
             .code(Code.fromAsset("lambdas/rest"))
@@ -47,19 +63,39 @@ public class ThetaTrimStack extends Stack {
             ))
             .build();
         restApi = RestApi.Builder.create(this, "RestAPI")
-            .restApiName("thetatrim")
+            .restApiName(prefix + "rest-api")
+            .build();
+        preProcessingQueue = Queue.Builder.create(this, "PreProcessingQueue")
+            .queueName(prefix + "pre-processing-queue")
             .build();
     }
-    
-    /** Configures and adds all endpoints to the Rest API. */
+
+    /**
+     * Configures and adds all endpoints to the Rest API.
+     */
     private void configureEndpoints() {
         Resource jobsResource = restApi.getRoot().addResource("jobs");
-        LambdaIntegration postJobIntegration = LambdaIntegration.Builder.create(postJobHandler).build();
+        LambdaIntegration postJobIntegration = LambdaIntegration.Builder.create(postJobLambda).build();
         jobsResource.addMethod("POST", postJobIntegration);
     }
-    
-    /** Grants all necessary for interaction between services. */
+
+    /**
+     * Configures all triggers between services.
+     */
+    private void configureTriggers() {
+        jobObjectBucket.addEventNotification(
+            EventType.OBJECT_CREATED,
+            new SqsDestination(preProcessingQueue),
+            NotificationKeyFilter.builder()
+                .prefix("")
+                .suffix("original.mp4")
+                .build());
+    }
+
+    /**
+     * Grants all necessary for interaction between services.
+     */
     private void grantPermissions() {
-        jobObjectBucket.grantReadWrite(postJobHandler);
+        jobObjectBucket.grantReadWrite(postJobLambda);
     }
 }
