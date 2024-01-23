cb78f14a692a7a6f61a2b5194d81d89362d223dc
Johannes Zottele <johannes.zottele@tuwien.ac.at>
2024-01-23 19:45:40 +0100

    Add video splitting functionality to preprocess script
    The preprocess script now includes functionality to split videos into chunks using FFMpeg. It also adds capability to download videos from S3, split them into chunks, and re-upload them back to S3. Temporary log statements are added to ensure correct functioning.

--- lambdas/video_processing/preprocess.py
+++ lambdas/video_processing/preprocess.py
@@ -2,13 +2,18 @@ import json
 import logging
 import boto3
 import os
+import subprocess
+import shlex
+import glob
 
 JOB_TABLE_NAME = os.environ["JOB_TABLE_NAME"]
+OBJ_BUCKET_NAME = os.environ["OBJECT_BUCKET_NAME"]
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 s3_deliminator = "%3A"
+s3_client = boto3.client('s3')
 
 
 def handler(event, context):
@@ -16,36 +21,62 @@ def handler(event, context):
   Handles the preprocessing of videos.
 
   Performs trimming and chunk splitting of the video.
-  TODO: extend docs
   """
+  # TODO: Replace by id from event
+  job_id = "test"
+  orig_video_key = "test_small.mp4"
 
   logger.info(f"Invoked with event: {event}")
 
-  db_client = boto3.client('dynamodb')
-  job_id, s3_key = extract_data(event, context)
+  tmp_files = glob.glob("/tmp/CHUNK-*")
+  logger.info(f"Current content of /tmp: {tmp_files}")
+  # video_url = get_source_video_url("test_small.mp4")
+  video_url = "/tmp/original.mp4"
 
-  logger.info(f"Job ID: {job_id}")
+  logger.info(f"Download video {orig_video_key} to {video_url}")
+  s3_client.download_file(OBJ_BUCKET_NAME, orig_video_key, video_url)
 
-  item = db_client.get_item(
-    TableName=JOB_TABLE_NAME,
-    Key={'PK': {'S': f"JOB#{job_id}"}, 'SK': {'S': "INFO"}}
-  )
+  ƒfmpeg_cmd = f"/opt/bin/ffmpeg -i {video_url} -segment_time 10 -acodec copy -f segment -vcodec copy -reset_timestamps 1 -map 0 /tmp/CHUNK-%d.mp4"
+  command1 = shlex.split(ƒfmpeg_cmd)
+  logger.info("Start video splitting")
+  p = subprocess.run(command1, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  logger.info("process stopped")
 
-  logger.info(f"Success")
+  tmp_files = glob.glob("/tmp/CHUNK-*")
+  logger.info(f"After split in /tmp: {tmp_files}")
 
-  # TODO: implement
-  chunks = [
-    {
-      'objectUrl': 'chunk1'
-    },
-    {
-      'objectUrl': 'chunk2'
-    }
-  ]
+  stdout = p.stdout.decode('utf-8')
+  stderr = p.stderr.decode('utf-8')
+
+  for line in stdout.split('\n'):
+    logger.info(line.strip())
+
+  for line in stderr.split('\n'):
+    logger.info(line.strip())
+
+  logger.info("Uploading chunks to S3")
+  chunks = []
+  for filepath in glob.glob('/tmp/CHUNK-*.mp4'):
+    filename = os.path.basename(filepath)
+    obj_key = f"{job_id}/{filename}"
+    s3_client.upload_file(filepath, OBJ_BUCKET_NAME, obj_key)
+    chunks.append({"objectUrl": obj_key})
+
+  logger.info("Chunks uploaded to S3")
 
   return {
     'chunks': chunks
   }
+
+
+def get_source_video_url(obj_key: str) -> str:
+  return s3_client.generate_presigned_url(
+    ClientMethod='get_object',
+    Params={
+      'Bucket': OBJ_BUCKET_NAME,
+      'Key': f"{obj_key}",
+    }
+  )
 
 
 def extract_data(event, context):
